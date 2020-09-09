5447e2a1d7f32f543fb989144b4c9450568bf2fd
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-09-09 18:19:48 +0700

    Updates

---
+++ precache-manifest.6cdcd4444dfe235a9bf39a158204e622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed48c6077aa7572aa9de77dc739b992a",
+    "url": "/markettracker-react_js-web/index.html"
+  },
+  {
+    "revision": "c8725eabce2a839ff723",
+    "url": "/markettracker-react_js-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "526f8bbc400f6c0377db",
+    "url": "/markettracker-react_js-web/static/js/2.d676bfb7.chunk.js"
+  },
+  {
+    "revision": "27200a5499d660fd8253e73b0c4cf016",
+    "url": "/markettracker-react_js-web/static/js/2.d676bfb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8725eabce2a839ff723",
+    "url": "/markettracker-react_js-web/static/js/main.9bdf4471.chunk.js"
+  },
+  {
+    "revision": "3d7df389d9f124b793d7",
+    "url": "/markettracker-react_js-web/static/js/runtime-main.481442b6.js"
+  }
+]);
